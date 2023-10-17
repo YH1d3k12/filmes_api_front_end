ed4b4efa776f1c1f0171948d5c820d0c810564d5
djalma <djalmahy@hotmail.com>
2023-10-17 02:40:40 -0300

    Feat: created search by winner

--- src/screens/listScreen.jsx
+++ src/screens/listScreen.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useDebugValue, useEffect, useState } from 'react';
 
 import Requester from '../data/requester.js';
 import Table from '../components/table.jsx';
 import NavigationButtons from '../components/navigation_buttons.jsx';
+import SearchByYear from '../utilities/searchByYear.js';
+import SearchByWinner from '../utilities/searchByWinner.js';
 
 import './listScreen.css';
 
@@ -13,34 +15,38 @@ export default function ListScreen() {
     const [currentPage, setCurrentPage] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
     const [filterYear, setFilterYear] = useState("");
-
-    // When the component is loaded useEffect is executed.
-    useEffect(() => {
-        // Calls fetchData when fully loaded.
-        fetchData();
-    }, [currentPage]);
-    // Second parameter of useEffect is a dependency list, if empty, the method is executed once.
-    // If given dependencies, useEffect is executed every time a value is changed.
+    const [filterWinner, setFilterWinner] = useState("");
 
     const fetchData = async () => {
         try {
+            // Adds filterYear to endpoint if its not empty.
+            const yearParam = filterYear ? `&year=${filterYear}` : ''; 
+            
+            let winnerParam = '';
+
+            // Checks the value of fliterWinner and adds it to the endpoint.
+            if (filterWinner === 'yes') {
+                winnerParam = '&winner=true';
+            } else if (filterWinner === 'no') {
+                winnerParam = '&winner=false';
+            }
+
             const data = await Requester(
-                'get', `?page=${currentPage}&size=15`, null,
+                'get', `?page=${currentPage}&size=15${winnerParam}${yearParam}`, 
+                null,
                 response => {
                     setTotalPages(response.data.totalPages);
-
                     return response.data;
                 }
             );
-
-            console.log('Data', data);
-
-            // Check if data.content is an array before setting the state.
+            
+            // Checks if the content of the response is an array.
             if (Array.isArray(data.content)) {
                 setMovieData(data.content);
             } else {
                 console.error('Its not an array:', data);
             }
+
         } catch (error) {
             console.error('Error fetching data:', error);
             throw new Error(error);
@@ -51,10 +57,11 @@ export default function ListScreen() {
         if (newPage >= 0 && newPage < totalPages) {
             setCurrentPage(newPage);
         } else if (newPage < 0) {
+            // Goes to the first page if newPage is less than zero.
             setCurrentPage(0);
         } else {
-            const filteredTotalPages = Math.ceil(filteredData.length / 15);
-            setCurrentPage(filteredTotalPages - 1);
+            // Goes to the last page if newPage is greater or equal than totalPages.
+            setCurrentPage(totalPages - 1);
         }
     };
 
@@ -67,40 +74,20 @@ export default function ListScreen() {
       );
 
     const CelYear = () => {
-        const handleYearChange = (e) => {
-            setFilterYear(e.target.value);
-        };
-    
-        return (
-            <div>
-                <p>Year</p>
-                <input 
-                    className="list-filter" 
-                    placeholder="Filter by year"
-                    value={filterYear}
-                    onChange={handleYearChange}
-                />
-            </div>
-        );
-    };
-
-    const CelWinner = () => {
         return (
             <div className="list-filter">
-                <p>Winner?</p>
-                <select className="list-filter">
-                    <option value="yes">Yes/No</option>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                </select>
+                <p>Year</p>
+                <SearchByYear value={filterYear} onChange={setFilterYear} />
             </div>
-        );
+        )
     };
 
-    const filteredData = movieData.filter(item => {
-        // Se o filtroYear estiver vazio ou o ano do filme coincidir com o filtro
-        return filterYear === "" || item.year.toString().includes(filterYear);
-    });
+    const CelWinner = () => (
+        <div className="list-filter">
+            <p>Winner?</p>
+            <SearchByWinner value={filterWinner} onChange={setFilterWinner} />
+        </div>
+    );
 
     // Defines the column headings and the values to be displayed.
     const table = [
@@ -110,11 +97,19 @@ export default function ListScreen() {
         { heading: <CelWinner />, rowName: 'winner' },
     ];
 
+    // When the component is loaded useEffect is executed.
+    useEffect(() => {
+        // Calls fetchData when fully loaded.
+        fetchData();
+    }, [currentPage, filterYear, filterWinner]);
+    // Second parameter of useEffect is a dependency list, if empty, the method is executed once.
+    // If given dependencies, useEffect is executed every time a value is changed.
+
     return (
         <div className="list-screen">
             <div className="list-screen-wrapper">
                 <h1>List Movies</h1>
-                <Table data={filteredData} table={table}></Table>
+                <Table data={movieData} table={table}></Table>
                 {renderNavigationButtons()}
             </div>
         </div>
---
+++ src/utilities/searchByYear.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import useDebounce from "./useDebounce"
+
+
+const SearchByYear = ({ value, onChange }) => {
+    const [filterYear, setFilterYear] = useState(value);
+    const debouncedChange = useDebounce(onChange, 500);
+
+    function handleChange(event) {
+        setFilterYear(event.target.value);
+        debouncedChange(event.target.value);
+    }
+
+    return (
+        <input
+            className="list-filter"
+            placeholder="Filter by year"
+            type="number"
+            value={filterYear}
+            onChange={handleChange}
+        />
+    )
+};
+
+
+export default SearchByYear;
